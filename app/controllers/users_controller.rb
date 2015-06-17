32d234a31245dd2c19e4feb9a2324de2a74bf914
Daria Woźnicka <woznicka.daria@gmail.com>
2015-06-17 16:03:30 +0200

    dodano controller i widok dla usera.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit 
+		@user=User.find(params[:id]) 
+	end 
+
+	def update 
+         @user=User.find(params[:id]) 
+         if @user.update(user_params) 
+            redirect_to root_url 
+         else 
+            render 'edit'
+         end 
+	end 
+
+	def destroy 
+         @user=User.find(params[:id]) 
+         @user.destroy 
+         redirect_to root_url 
+	end 
 
 	private 
 
